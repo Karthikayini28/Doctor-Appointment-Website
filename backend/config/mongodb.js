ea34c49ffb7caba3762c09e1b718141c31279a27
Karthikayini28 <karthisr.wiselearnz@gmail.com>
2025-08-22 17:47:39 +0530

    Initial commit

---
+++ backend/config/mongodb.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  mongoose.connection.on('connected', () => console.log('Database Connected'))
+  await mongoose.connect('mongodb://127.0.0.1:27017/doctor')
+}
+
+export default connectDB
